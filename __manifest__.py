27f4dbe135937df438def7da1de618d713e0f958
acordobag <amcordobag@gmail.com>
2017-08-31 23:11:51 -0600

    First commit

---
+++ __manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Custom Invoices",
+
+    'summary': """Companys invoices Costa rica""",
+
+    'description': """
+
+    """,
+
+    'author': "Adrian Cordoba",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'account_invoices',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','account'],
+
+    # always loaded
+    'data': [
+        'reports/consurba_invoice.xml',
+    ]
+}
